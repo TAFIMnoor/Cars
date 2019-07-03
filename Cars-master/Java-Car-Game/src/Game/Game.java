ddc9d7fd15bb3c2e8a267f75474028e585c5c747
TAFIMnoor <43815369+TAFIMnoor@users.noreply.github.com>
2019-07-03 23:18:06 +0600

    Add files via upload

---
+++ Cars-master/Java-Car-Game/src/Game/Game.java
@@ -0,0 +1,10 @@
+package Game;
+
+public class Game {
+
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+    }
+    
+}
